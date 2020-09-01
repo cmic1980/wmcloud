2002f0db729d9da0f1ec1c612402a00afd947dd3
yumiao <miao.yu@ctx.sg>
2020-09-01 17:47:15 +0800

    add requirements.txt

--- wmcloud/settings.py
+++ wmcloud/settings.py
@@ -64,7 +64,6 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'wmcloud.pipelines.JsonWriterPipeline': 300,
-    'wmcloud.pipelines.MysqlWriterPipeline': 800,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
